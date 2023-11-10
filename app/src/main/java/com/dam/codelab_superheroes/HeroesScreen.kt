16fee44a46612622178ccb45c5dd7f45c74ae806
Juan Valera Reales <juanvalerareales@hotmail.com>
2023-11-10 17:39:06 +0100

    Terminado

--- app/src/main/java/com/dam/codelab_superheroes/HeroesScreen.kt
+++ app/src/main/java/com/dam/codelab_superheroes/HeroesScreen.kt
@@ -34,6 +34,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -42,24 +43,31 @@ import androidx.compose.ui.unit.sp
 import com.dam.codelab_superheroes.model.Hero
 import com.dam.codelab_superheroes.model.HeroesRepository.heroes
 import com.dam.codelab_superheroes.ui.theme.SuperheroesTheme
+import com.dam.codelab_superheroes.ui.theme.hero_card_color_1
+import com.dam.codelab_superheroes.ui.theme.hero_card_color_2
+import com.dam.codelab_superheroes.ui.theme.hero_card_color_3
+import com.dam.codelab_superheroes.ui.theme.hero_card_color_4
+import com.dam.codelab_superheroes.ui.theme.hero_card_color_5
+import com.dam.codelab_superheroes.ui.theme.hero_card_color_6
+import com.dam.codelab_superheroes.ui.theme.hero_name_color_1
+import com.dam.codelab_superheroes.ui.theme.hero_name_color_2
+import com.dam.codelab_superheroes.ui.theme.hero_name_color_3
+import com.dam.codelab_superheroes.ui.theme.hero_name_color_4
+import com.dam.codelab_superheroes.ui.theme.hero_name_color_5
+import com.dam.codelab_superheroes.ui.theme.hero_name_color_6
 
-/*TODO
-   - Cambiar el color de la card en stats en función del heroId
-   - Cambiar tipografías
-   - Cambiar Herostats para que muestre los recursos correspondientes
- */
 
 @Preview(showBackground = false, showSystemUi = false)
 @Composable
 fun PreviewHeroItem() {
     SuperheroesTheme {
-        HeroList(Modifier.padding(horizontal = 16.dp, vertical = 8.dp))
+        HeroList()
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HeroList(modifier: Modifier = Modifier) {
+fun HeroList() {
     var heroId by remember { mutableStateOf(0) }
     val callbackHeroId: (Int) -> Unit = { id ->
         heroId = id
@@ -75,49 +83,17 @@ fun HeroList(modifier: Modifier = Modifier) {
                 items(heroes) {
                     HeroItem(
                         it,
-                        modifier,
+                        Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
                         callbackHeroId
                     )
                 }
             }
         } else {
-            HeroStats(heroId, modifier, callbackToMain)
+            HeroStats(heroId, Modifier.padding(horizontal = 16.dp), callbackToMain)
         }
 
     }
 }
-
-@Composable
-fun HeroStats(heroId: Int, modifier: Modifier, callbackToMain: () -> Unit) {
-    Card(
-        modifier = modifier.padding(top=150.dp),
-        elevation = CardDefaults.cardElevation(2.dp)
-    ) {
-        Column(
-            modifier = Modifier.padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.stats_1),
-                contentDescription = null,
-            )
-            Row(horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment=Alignment.CenterVertically,
-                modifier=Modifier.padding(top=16.dp).fillMaxWidth()) {
-                Image(
-                    modifier = Modifier.size(60.dp),
-                    painter = painterResource(id = R.drawable.android_superhero1),
-                    contentDescription = null
-                )
-                Text(text = stringResource(id = R.string.hero1))
-                Button(onClick = callbackToMain) {
-                    Text(text = stringResource(id = R.string.back_to_main))
-                }
-            }
-        }
-    }
-}
-
 
 @Composable
 fun HeroItem(
@@ -126,7 +102,6 @@ fun HeroItem(
     callbackHeroId: (Int) -> Unit
 ) {
     var expanded by remember { mutableStateOf(false) }
-
 
 
     Card(
@@ -181,9 +156,115 @@ fun HeroItem(
 
 
 @Composable
+fun HeroStats(heroId: Int, modifier: Modifier, callbackToMain: () -> Unit) {
+    var statsPictureRes = 0
+    var heroPictureRes = 0
+    var heroNameRes = 0
+    var colorCard = Color(1)
+    var colorName = Color(1)
+    when (heroId) {
+        1 -> {
+            statsPictureRes = R.drawable.stats_1
+            heroPictureRes = R.drawable.android_superhero1
+            heroNameRes = R.string.hero1
+            colorCard = hero_card_color_1
+            colorName = hero_name_color_1
+        }
+
+        2 -> {
+            statsPictureRes = R.drawable.stats_2
+            heroPictureRes = R.drawable.android_superhero2
+            heroNameRes = R.string.hero2
+            colorCard = hero_card_color_2
+
+            colorName = hero_name_color_2
+        }
+
+        3 -> {
+            statsPictureRes = R.drawable.stats_3
+            heroPictureRes = R.drawable.android_superhero3
+            heroNameRes = R.string.hero3
+            colorCard = hero_card_color_3
+            colorName = hero_name_color_3
+        }
+
+        4 -> {
+            statsPictureRes = R.drawable.stats_4
+            heroPictureRes = R.drawable.android_superhero4
+            heroNameRes = R.string.hero4
+            colorCard = hero_card_color_4
+            colorName = hero_name_color_4
+        }
+
+        5 -> {
+            statsPictureRes = R.drawable.stats_5
+            heroPictureRes = R.drawable.android_superhero5
+            heroNameRes = R.string.hero5
+            colorCard = hero_card_color_5
+            colorName = hero_name_color_5
+        }
+
+        6 -> {
+            statsPictureRes = R.drawable.stats_6
+            heroPictureRes = R.drawable.android_superhero6
+            heroNameRes = R.string.hero6
+            colorCard = hero_card_color_6
+            colorName = hero_name_color_6
+        }
+    }
+    Card(
+        modifier = modifier.padding(top = 150.dp),
+        elevation = CardDefaults.cardElevation(2.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = colorCard,
+        ),
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = statsPictureRes),
+                contentDescription = null,
+            )
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .padding(top = 16.dp)
+                    .fillMaxWidth()
+            ) {
+                Image(
+                    modifier = Modifier
+                        .size(60.dp)
+                        .padding(start = 8.dp),
+                    painter = painterResource(id = heroPictureRes),
+                    contentDescription = null
+                )
+                Text(
+                    text = stringResource(id = heroNameRes),
+                    style = MaterialTheme.typography.displaySmall,
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(start = 8.dp),
+                    color = colorName
+                )
+                Button(onClick = callbackToMain, modifier = Modifier.padding(end = 8.dp)) {
+                    Text(text = stringResource(id = R.string.back_to_main))
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
 fun AdditionalInfo(hero: Hero, modifier: Modifier = Modifier, callbackHeroId: (Int) -> Unit) {
     Row(modifier = modifier) {
-        Text(text = stringResource(id = R.string.power))
+        Text(
+            text = stringResource(id = R.string.power),
+            modifier = Modifier.padding(end = 8.dp)
+        )
         Text(text = stringResource(id = hero.powerRes))
         Spacer(modifier = Modifier.weight(1f))
         OutlinedButton(onClick = { callbackHeroId(hero.heroId) }) {
